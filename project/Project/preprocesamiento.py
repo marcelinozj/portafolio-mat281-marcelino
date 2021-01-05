044fedf3d9756d80261e158ff585e26bd1dcbe8d
marcelino <marcelino.zuniga@sansano.usm.cl>
2021-01-04 22:30:10 -0600

    entrega_de_proyecto

--- project/Project/preprocesamiento.py
+++
@@ -1,1 +0,0 @@
-hacer la mejor predicción de cada imagen. Para ellos es necesario realizar los pasos clásicos de un proyecto de _Machine Learning_, como estadística descriptiva, visualización y preprocesamiento.
